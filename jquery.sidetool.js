4cf8d19847b3e0afdeb6f0d8da14acbd11e88f42
amano225 <amp07642@gmail.com>
2013-10-20 18:40:36 +0900

    jQueryライブラリの基本テンプレート

--- jquery.sidetool.js
+++ jquery.sidetool.js
@@ -1,2 +1,12 @@
 // サイドからの通知用jQueryプラグイン
 // jQuery.sidetool.js
+(function($) {
+	$.fn.sidetool = function(opt) {
+		var def = {
+			'side': 'bottom',
+			'viewBtn': null
+		}
+		var set = $.extend(def, opt);
+		return (this);
+	}
+} (jQuery));
